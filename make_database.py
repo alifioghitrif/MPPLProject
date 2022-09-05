1c1f689677aa12f577ff4b375b2df5a8951a25af
rizal-mujahiddan <rizal.mujahiddan@gmail.com>
2022-09-05 11:47:41 +0700

    python data

--- make_database.py
+++ make_database.py
@@ -1,5 +1,17 @@
-from sqlalchemy import create_engine,Table,Column,Integer,String,MetaData,Date
+from sqlalchemy import (
+    create_engine,
+    Table,
+    Column,
+    Integer,
+    String,
+    MetaData,
+    Date,
+    ForeignKey,
+)
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy_utils import *
+
+Base = declarative_base()
 
 engine = create_engine("mysql+pymysql://root:@localhost/mppl")
 if not database_exists(engine.url):
@@ -7,18 +19,41 @@ if not database_exists(engine.url):
 
 meta = MetaData()
 
-penduduk = Table(
-    'penduduk',meta,
-    Column('Id',Integer,autoincrement=True),
-    Column('NIK',String(16),primary_key=True),
-    Column('Nomor KK',String(16)),
-    Column('Jenis Kelamin',String(32)),
-    Column('Status Perkawinan',String(32)),
-    Column('Nomor Rumah',String(9)),
-    Column('Tanggal Lahir',Date),
-    Column('Pekerjaan',String(64)),
-    Column('Status Dalam Keluarga',String(64)),
-    Column('Nomor Telepon',String(16))
-)
+wargadesa = Table("WargaDesa", meta, relationship(secondary=a))
+
+
+class wargadesa(Base):
+    __tablename__ = "WargaDesa"
+    WargaID = Column(Integer, autoincrement=True, primary_key=True)
+    NIK = Column(String(16))
+    Nama = Column(String(64))
+    Nomor_KK = Column(String(16))
+    Jenis_Kelamin = Column(String(32))
+    Status_Perkawinan = Column(String(32))
+    Tanggal_Lahir = Column(Date)
+    Pekerjaan = Column(String(64))
+    Status_Dalam_Keluarga = Column(String(64))
+    Nomor_Telepon = Column(String(16))
+
+
+class dusun(Base):
+    __tablename__ = "Dusun"
+    DusunID = Column(Integer, autoincrement=True, primary_key=True)
+    Nama_Dusun = Column(String(64))
+
+
+class aparat_desa(Base):
+    __tablename__ = "AparatDesa"
+    AparatID = Column(Integer, autoincrement=True, primary_key=True)
+    Nama = Column(String(64))
+    Email = Column(String(128))
+
+
+class aparatwarga(Base):
+    __tablename__ = "Aparat_warga_assoc"
+    aparatwargaid = Column(Integer, primary_key=True)
+    aparat_id = Column(ForeignKey("AparatDesa.AparatID"))
+    warga_id = Column(ForeignKey("WargaDesa.WargaID"))
+
 
 meta.create_all(engine)
